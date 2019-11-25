1afdc830e6aa3ec4688cd35953ca10400727a967
huangjingchao <jingchaoh@tujia.com>
2019-11-25 13:41:31 +0800

    fix ios

--- ios/Classes/SwiftFlutterWallePlugin.swift
+++ ios/Classes/SwiftFlutterWallePlugin.swift
@@ -3,7 +3,7 @@ import UIKit
 
 public class SwiftFlutterWallePlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "flutter_walle_plugin", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: "plugins.hjc.com/flutter_walle_plugin", binaryMessenger: registrar.messenger())
     let instance = SwiftFlutterWallePlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
